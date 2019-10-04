fb8686e7786d966d0e3977d3c950a7f156f9207a
fancyaction <labcoatdisaster@gmail.com>
2019-10-04 17:18:40 -0500

    Setup project

---
+++ src/Components/LogoChallenge.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LogoChallenge extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
