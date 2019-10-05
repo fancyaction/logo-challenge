46d75bdaf1d1d283e0d361e5a7bd11adb2987079
fancyaction <labcoatdisaster@gmail.com>
2019-10-05 10:17:37 -0500

    Move logo to new file, update resetGame

---
+++ src/Components/Logo.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../assets/img/ia-logo-back.png';
+
+const useStyles = makeStyles(theme => ({
+    logoContainer: {
+        position: 'relative',
+        '& img': { zIndex: '-1', width: '100%', display: 'block' }
+    },
+    location1: {
+        border: '5px solid red',
+        position: 'absolute',
+        width: '22%',
+        height: '22%',
+        borderRadius: '50%',
+        transform: 'translate(171%, 13%)'
+    },
+    location2: {
+        border: '5px solid red',
+        position: 'absolute',
+        width: '22%',
+        height: '22%',
+        borderRadius: '50%',
+        transform: 'translate(296%, 29%)'
+    },
+    location3: {
+        border: '5px solid red',
+        position: 'absolute',
+        width: '22%',
+        height: '22%',
+        borderRadius: '50%',
+        transform: 'translate(271%, 180%)'
+    },
+    location4: {
+        border: '5px solid red',
+        position: 'absolute',
+        width: '22%',
+        height: '22%',
+        borderRadius: '50%',
+        transform: 'translate(80%, 213%)'
+    },
+    location5: {
+        border: '5px solid red',
+        position: 'absolute',
+        width: '22%',
+        height: '22%',
+        borderRadius: '50%',
+        transform: 'translate(13%, 113%)'
+    }
+}));
+
+const answers = {
+    location1: 'nodeBlue',
+    location2: 'nodeRed',
+    location3: ['nodeBlack', 'nodeBlack2'],
+    location4: 'nodeGreen',
+    location5: ['nodeBlack', 'nodeBlack2']
+};
+
+const Logo = ({ active, updateScore }) => {
+    const classes = useStyles();
+
+    const drop = ev => {
+        ev.preventDefault();
+        const location = ev.target.id;
+        const data = ev.dataTransfer.getData(active);
+
+        if (answers[location].includes(active) || answers[location] === active) {
+            ev.target.appendChild(document.getElementById(data));
+            updateScore(location);
+        }
+    };
+
+    return (
+        <div className={classes.logoContainer} onDrop={drop} onDragOver={ev => ev.preventDefault()}>
+            <div id="location1" className={classes.location1}></div>
+            <div id="location2" className={classes.location2}></div>
+            <div id="location3" className={classes.location3}></div>
+            <div id="location4" className={classes.location4}></div>
+            <div id="location5" className={classes.location5}></div>
+            <img id="logo" src={logo} alt="Logo" />
+        </div>
+    );
+};
+
+export default Logo;
--- src/Components/LogoChallenge.js
+++ src/Components/LogoChallenge.js
@@ -5,7 +5,7 @@ import Header from './Header';
 import ResetButton from './ResetButton';
 import ScoreBox from './ScoreBox';
 import Nodes from './Nodes';
-import logo from '../assets/img/ia-logo-back.png';
+import Logo from './Logo';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -15,80 +15,7 @@ const useStyles = makeStyles(theme => ({
         alignItems: 'center',
         flexDirection: 'column'
     },
-    logoContainer: {
-        position: 'relative',
-        '& img': { zIndex: '-1', width: '100%', display: 'block' }
-    },
-    location1: {
-        border: '5px solid red',
-        position: 'absolute',
-        width: '22%',
-        height: '22%',
-        borderRadius: '50%',
-        transform: 'translate(171%, 13%)'
-    },
-    location2: {
-        border: '5px solid red',
-        position: 'absolute',
-        width: '22%',
-        height: '22%',
-        borderRadius: '50%',
-        transform: 'translate(296%, 29%)'
-    },
-    location3: {
-        border: '5px solid red',
-        position: 'absolute',
-        width: '22%',
-        height: '22%',
-        borderRadius: '50%',
-        transform: 'translate(271%, 180%)'
-    },
-    location4: {
-        border: '5px solid red',
-        position: 'absolute',
-        width: '22%',
-        height: '22%',
-        borderRadius: '50%',
-        transform: 'translate(80%, 213%)'
-    },
-    location5: {
-        border: '5px solid red',
-        position: 'absolute',
-        width: '22%',
-        height: '22%',
-        borderRadius: '50%',
-        transform: 'translate(13%, 113%)'
-    }
 }));
-
-const answers = {
-    location1: 'nodeBlue',
-    location2: 'nodeRed',
-    location3: ['nodeBlack', 'nodeBlack2'],
-    location4: 'nodeGreen',
-    location5: ['nodeBlack', 'nodeBlack2']
-}
-
-const drop = active => ev => {
-    const location = ev.target.id;
-    ev.preventDefault();
-    const data = ev.dataTransfer.getData(active);
-
-    if (answers[location].includes(active) || answers[location] === active) {
-        ev.target.appendChild(document.getElementById(data));
-    }
-};
-
-const Logo = ({ classes, active }) => (
-    <div className={classes.logoContainer} onDrop={drop(active)} onDragOver={ev => ev.preventDefault()}>
-        <div id="location1" className={classes.location1}></div>
-        <div id="location2" className={classes.location2}></div>
-        <div id="location3" className={classes.location3}></div>
-        <div id="location4" className={classes.location4}></div>
-        <div id="location5" className={classes.location5}></div>
-        <img id="logo" src={logo} alt="Logo" />
-    </div>
-);
 
 const LogoChallenge = () => {
     const classes = useStyles();
@@ -103,14 +30,7 @@ const LogoChallenge = () => {
     const [active, setActive] = React.useState();
 
     const resetGame = () => {
-        setScore(0);
-        setLocations({
-            location1: false,
-            location2: false,
-            location3: false,
-            location4: false,
-            location5: false
-        });
+        window.location.reload();
     };
 
     const updateScore = location => {
@@ -125,7 +45,7 @@ const LogoChallenge = () => {
                 <Header />
                 <ResetButton resetGame={resetGame} />
                 <ScoreBox score={score} />
-                <Logo classes={classes} active={active} setLocations={setLocations} setScore={setScore} />
+                <Logo active={active} updateScore={updateScore} />
                 <Nodes locations={locations} setActive={setActive} />
             </Paper>
         </Container>
