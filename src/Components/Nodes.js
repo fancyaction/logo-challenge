0c6df491e5cc9c3cb967e69ec490ef03a694340c
fancyaction <labcoatdisaster@gmail.com>
2019-10-05 11:13:54 -0500

    Add cursor to style

--- src/Components/Nodes.js
+++ src/Components/Nodes.js
@@ -5,10 +5,8 @@ import nodeGreen from '../assets/img/ia-logo-dot-green.png';
 import nodeRed from '../assets/img/ia-logo-dot-red.png';
 
 const Nodes = ({ locations, setActive }) => {
-
     const onDragStart = ev => {
         const nodeName = ev.target.id;
-        console.log('👀: Nodes -> nodeName', nodeName);
 
         setActive(nodeName);
         ev.dataTransfer.setData(nodeName, nodeName);
@@ -21,6 +19,7 @@ const Nodes = ({ locations, setActive }) => {
                 src={nodeBlack}
                 alt="Black Node"
                 draggable={!locations.location3 || !locations.location5}
+                style={{ cursor: !locations.location3 || !locations.location5 ? 'grab' : 'not-allowed' }}
                 onDragStart={onDragStart}
             />
             <img
@@ -28,6 +27,7 @@ const Nodes = ({ locations, setActive }) => {
                 src={nodeBlack}
                 alt="Black Node Two"
                 draggable={!locations.location3 || !locations.location5}
+                style={{ cursor: !locations.location3 || !locations.location5 ? 'grab' : 'not-allowed' }}
                 onDragStart={onDragStart}
             />
             <img
@@ -35,6 +35,8 @@ const Nodes = ({ locations, setActive }) => {
                 src={nodeBlue}
                 alt="Blue Node"
                 draggable={!locations.location1}
+                style={{ cursor: !locations.location1 ? 'grab' : 'not-allowed' }}
+
                 onDragStart={onDragStart}
             />
             <img
@@ -42,6 +44,7 @@ const Nodes = ({ locations, setActive }) => {
                 src={nodeGreen}
                 alt="Green Node"
                 draggable={!locations.location4}
+                style={{ cursor: !locations.location4 ? 'grab' : 'not-allowed' }}
                 onDragStart={onDragStart}
             />
             <img id="nodeRed" src={nodeRed} alt="Red Node" draggable={!locations.location2} onDragStart={onDragStart} />
